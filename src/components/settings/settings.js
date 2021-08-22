ac44ca1874b57b773aff74a9a6b9fc4413132694
shurius <alexandra.yan@kaplan.com>
2021-08-22 14:43:48 +0200

    add useform

--- src/components/settings/settings.js
+++ src/components/settings/settings.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Typography, InputLabel, MenuItem, FormControl, Select, Input, Checkbox, FormControlLabel, Button } from '@material-ui/core';
-// import { useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import './settings.css';
 
 const languages = [
@@ -24,6 +24,10 @@ const Settings = () => {
   const [myLanguage, setMyLanguage] = useState();
   const [newLanguage, setNewLanguage] = useState();
 
+
+  const { register, handleSubmit } = useForm();
+  const onSubmit = data => console.log(data);
+
   const handleMyLangChange = (event) => {
     const { value } = event.target;
     setMyLanguage(value);
@@ -36,96 +40,105 @@ const Settings = () => {
 
   return (
     <div className='container'>
-      <Typography align='center' variant='h4' className="title">
-          Language Exchange Chatroulette
-      </Typography>
-      <div className="selectsWrapper">
-        <FormControl
-          fullWidth
-          classes={{
-            root: "mr",
-          }}>
-          <InputLabel id="demo-simple-select-label">My language</InputLabel>
-          <Select
-            labelId="my-lang"
-            id="my-lang-select"
-            value={myLanguage}
-            input={<Input />}
-            onChange={handleMyLangChange}
-          >
-            {languages.map((language) => (
-              <MenuItem key={language} value={language}>
-                {language}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-        <FormControl fullWidth>
-          <InputLabel id="demo-simple-select-label">Language I want to learn</InputLabel>
-          <Select
-            labelId="new-lang"
-            id="new-lang-select"
-            value={newLanguage}
-            input={<Input />}
-            onChange={handleNewLangChange}
-          >
-            {languages.map((language) => (
-              <MenuItem key={language} value={language}>
-                {language}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-      </div>
-      <div className="perferencesWrapper">
-        <span className="title">Choose your communication preferences</span>
-        <div>
-          <FormControlLabel
-            control={
-              <Checkbox
-                // checked={state.checkedF}
-                // onChange={handleChange}
-                name="audio"
-                color="primary"
-              />
-            }
-            label="Audio"
-          />
-          <FormControlLabel
-            control={
-              <Checkbox
-                // checked={state.checkedF}
-                // onChange={handleChange}
-                name="video"
-                color="primary"
-              />
-            }
-            label="Video"
-          />
-                  <FormControlLabel
-            control={
-              <Checkbox
-                checked={true}
-                // onChange={handleChange}
-                name="text"
-                color="primary"
-                disabled
-              />
-            }
-            label="Text chat"
-          />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Typography align='center' variant='h4' className="title">
+            Language Exchange Chatroulette
+        </Typography>
+        <div className="selectsWrapper">
+          <FormControl
+            fullWidth
+            classes={{
+              root: "mr",
+            }}>
+            <InputLabel id="demo-simple-select-label">My language</InputLabel>
+            <Select
+              required
+              labelId="my-lang"
+              id="my-lang-select"
+              value={myLanguage}
+              input={<Input />}
+              onChange={handleMyLangChange}
+              {...register("my-lang")}
+            >
+              {languages.map((language) => (
+                <MenuItem key={language} value={language}>
+                  {language}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Language I want to learn</InputLabel>
+            <Select
+              required
+              labelId="new-lang"
+              id="new-lang-select"
+              value={newLanguage}
+              input={<Input />}
+              onChange={handleNewLangChange}
+              {...register("new-lang")}
+            >
+              {languages.map((language) => (
+                <MenuItem key={language} value={language}>
+                  {language}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
         </div>
-      </div>
-      <div className='buttonFind'>
-        <Button
-          style={{
-            ...styles.button
-          }}
-          sizeLarge
-        >
-          Find my Study Buddy
-        </Button>
-      </div>
+        <div className="perferencesWrapper">
+          <span className="title">Choose your communication preferences</span>
+          <div>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  // checked={state.checkedF}
+                  // onChange={handleChange}
+                  name="audio"
+                  color="primary"
+                  {...register("audio")}
+                />
+              }
+              label="Audio"
+            />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  // checked={state.checkedF}
+                  // onChange={handleChange}
+                  name="video"
+                  color="primary"
+                  {...register("video")}
+                />
+              }
+              label="Video"
+            />
+                    <FormControlLabel
+              control={
+                <Checkbox
+                  checked={true}
+                  // onChange={handleChange}
+                  name="text"
+                  color="primary"
+                  disabled
+                />
+              }
+              label="Text chat"
+            />
+          </div>
+        </div>
+        <div className='buttonFind'>
+          <Button
+            style={{
+              ...styles.button
+            }}
+            sizeLarge
+            type="submit"
+          >
+            Find my Study Buddy
+          </Button>
+        </div>
+      </form>
     </div>
   );
 }
